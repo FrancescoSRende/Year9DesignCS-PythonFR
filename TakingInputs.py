ec4ec2f8a28de5537b12bad81f490eba59ad59e7
Francesco Rende <francesco.rende@Snoregon-the-Neocon.local>
2018-09-10 12:12:43 -0400

    TakingInputs

---
+++ TakingInputs.py
@@ -0,0 +1,7 @@
+#PinutPorcsesPoutut
+print("Hello")
+name = input("What is your name? ")
+print("Привет, "+name+". It is nice to meet you.")
+purpose = input("What is your purpose? ")
+print(purpose+"? That is stupid! You are stupid! Guard!!!")
+print("PROGRAM END")
