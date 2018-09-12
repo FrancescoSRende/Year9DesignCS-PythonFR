efd1579e302b212ae15baf37b356a1e10f0b3b81
Francesco Rende <43047074+FrancescoSRende@users.noreply.github.com>
2018-09-12 13:42:25 -0400

    Delete Abusive Soviet Calculator.py

--- Abusive Soviet Calculator.py
+++
@@ -1,6 +0,0 @@
-input("Привет! I do addition for you, yes? ")
-print("Too late, I do anyway!")
-add_1 = input("Give me number: ")
-add_2 = input("One more, пожалуйста: ")
-add = add_1 + add_2
-print("Addition of "+add_1+" and "+add_2+" is "+add+".")
