5ea92817d83d388adc097faf809eff634480f725
Francesco Rende <francesco.rende@Snoregon-the-Neocon.local>
2018-09-18 14:10:06 -0400

    Newer, improveder and Sovieter

--- AbusiveSovietCalculator.py
+++ AbusiveSovietCalculator.py
@@ -6,14 +6,13 @@ add_2 = input("One more, пожалуйста: ")
 add_1 = float(add_1)
 add_2 = float(add_2)
 sum = add_1 + add_2
-sum = str(sum)
-print("Addition of "+add_1+" and "+add_2+" is "+sum+".")
+print("Addition of",add_1," and",add_2," is "+str(sum)+".")
 times_1 = input("Now I do multiplication of 3 numbers! Give me first one: ")
 times_2 = input("Another: ")
 times_3 = input("Last one, пожалуйста: ")
 times_1 = float(times_1)
 times_2 = float(times_2)
 times_3 = float(times_3)
-prod = times_1*times_2*times_3
-print("Your answer of multiplication is "+str(prod)+".")
+product = times_1 * times_2 * times_3
+print("Your answer of multiplication is "+str(product)+".")
 print("PROGRAM IS END")
