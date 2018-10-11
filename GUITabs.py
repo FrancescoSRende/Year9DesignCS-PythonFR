12c6433663ee7d6c2b84ac85e10a60fabaf3d284
Francesco Rende <francesco.rende@Snoregon-the-Neocon.local>
2018-10-11 15:29:41 -0400

    Different things under different tabs

--- GUITabs.py
+++ GUITabs.py
@@ -1,6 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
 
+def clicked():
+	print("Button clicked")
+
+def hitReturn(event):
+	print("User pressed return")
+
 root = tk.Tk()
 root.title("GUI Tabs")
 
@@ -13,5 +19,12 @@ tabControl.pack(expand=1, fill="both")
 tab2 = ttk.Frame(tabControl)
 tabControl.add(tab2, text="Tab 2")
 tabControl.pack(expand=1, fill="both")
+
+entry1tab1 = tk.Entry(tab1)
+entry1tab1.pack()
+entry1tab1.bind("<Return>",hitReturn)
+
+button1tab2 = tk.Button(tab2, text="Button2", command=clicked)
+button1tab2.pack()
 
 root.mainloop()
